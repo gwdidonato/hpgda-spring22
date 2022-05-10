3d1643c425148353889703eee7e4a2292812b834
Guido Walter Di Donato <gw.didonato@gmail.com>
2022-05-11 00:58:48 +0200

    removed old src

--- cuda_CONTEST/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run vector sum;
-bin/b -d -c -n 100000000 -b vec -I 1 -i 30 -t 64;
-bin/b -d -c -n 100000000 -b vec -I 2 -i 30 -t 64;
-
-# Run matrix multiplication;
-bin/b -d -c -n 1000 -b mmul -I 1 -i 30 -t 8;
-bin/b -d -c -n 1000 -b mmul -I 2 -i 30 -t 8 -B 14;
